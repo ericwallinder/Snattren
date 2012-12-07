8e4246ae87ed5d8ad676765850df7029733fee19
ericwallinder <eric.wallinder@gmail.com>
2012-12-08 00:08:49 +0100

    Corrected the syntax in the json file and started to use the ConfigParser from the main method.

--- src/se/snattren/Snattren.java
+++ src/se/snattren/Snattren.java
@@ -1,17 +1,28 @@
 package se.snattren;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import com.google.gson.Gson;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class Snattren {
-	public static void main(String[] args) {
-		Injector injector = Guice.createInjector(new AbstractModule() {
-			@Override
-			protected void configure() {
-				
-			}
-		});
-		ArgsParser argumentParser = injector.getInstance(ArgsParser.class);
+	public static void main(String[] args) throws IOException {
+//		Injector injector = Guice.createInjector(new AbstractModule() {
+//			@Override
+//			protected void configure() {
+//
+//			}
+//		});
+//		ArgsParser argumentParser = injector.getInstance(ArgsParser.class);
+        ConfigParser configParser = new ConfigParser(new Gson());
+        URL url = Resources.getResource("config.json");
+        String json = Resources.toString(url, Charsets.UTF_8);
+        Config config = configParser.parse(json);
+        System.out.println(config.getPort());
 	}
 }
